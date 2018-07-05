fde6226b845750ec194676b19ac085cb14475d3f
Paulo Alves <juninhpaulo@hotmail.com>
2018-07-05 02:07:34 -0300

    updating perfil

--- src/components/perfil.jsx
+++ src/components/perfil.jsx
@@ -4,46 +4,56 @@ import Social from './social'
 export default class Perfil extends Component {
 
   render() {
-      return (
-        <section id="home" class="s-home page-hero target-section" data-parallax="scroll" data-image-src="images/hero-bg.jpg" data-natural-width="3000" data-natural-height="2000" data-position-y="center">
 
-              <div class="overlay"></div>
-              <div class="shadow-overlay"></div>
+    let perfil = {
+        title: 'Hello World',
+        description: `
+        I am Paulo Alves.
+        I am a fullstack developer
+        artificial intelligence enthusiast.
+        `
+    }
 
-              <div class="home-content">
+    return (
+      <section id="home" class="s-home page-hero target-section" data-parallax="scroll" data-image-src="images/hero-bg.jpg" data-natural-width="3000" data-natural-height="2000" data-position-y="center">
 
-                  <div class="row home-content__main">
+            <div class="overlay"></div>
+            <div class="shadow-overlay"></div>
 
-                      <h3>Hello World</h3>
+            <div class="home-content">
 
-                      <h1>
-                          I am Paulo Alves. <br/>
-                          I am a fullstack developer <br/>
-                          artificial intelligence enthusiast.
-                      </h1>
+                <div class="row home-content__main">
 
-                      <div class="home-content__buttons">
-                          <a href="#works" class="smoothscroll btn btn--stroke">
-                              Latest Projects
-                          </a>
-                          <a href="#about" class="smoothscroll btn btn--stroke">
-                              More About Me
-                          </a>
-                      </div>
+                    <h3>
+                      {perfil.title}
+                    </h3>
 
-                      <div class="home-content__scroll">
-                          <a href="#about" class="scroll-link smoothscroll">
-                              <span>Scroll Down</span>
-                          </a>
-                      </div>
+                    <h1>
+                      {perfil.description}
+                    </h1>
 
-                  </div>
+                    <div class="home-content__buttons">
+                        <a href="#works" class="smoothscroll btn btn--stroke">
+                            Latest Projects
+                        </a>
+                        <a href="#about" class="smoothscroll btn btn--stroke">
+                            More About Me
+                        </a>
+                    </div>
 
-              </div>
+                    <div class="home-content__scroll">
+                        <a href="#about" class="scroll-link smoothscroll">
+                            <span>Scroll Down</span>
+                        </a>
+                    </div>
 
-              <Social/>
+                </div>
 
-          </section>
-      )
+            </div>
+
+            <Social/>
+
+        </section>
+    )
   }
 }

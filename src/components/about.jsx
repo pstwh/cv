507458aac37de1918979d968ce9a0da916eab48f
Paulo Alves <juninhpaulo@hotmail.com>
2018-07-05 02:26:27 -0300

    adding content

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Content from './content'
 import Skills from './skills'
 
 export default props => (
@@ -13,19 +14,8 @@ export default props => (
         </div>
 
         <div class="row about-content">
-
-            <div class="col-six tab-full left">
-                <h3>Howdy!</h3>
-
-                <p>Lorem ipsum Ut eiusmod ex magna sit dolor esse adipisicing minim ad cupidatat eu veniam nostrud mollit laboris sunt magna velit culpa consectetur nostrud consectetur labore sed do.</p>
-
-                <p>
-                Lorem ipsum Nisi officia Duis irure voluptate dolor commodo pariatur occaecat aliquip adipisicing voluptate Ut in qui ea sint occaecat in commodo in in in incididunt ut sunt in Ut Duis in ut ex qui anim cupidatat cupidatat ex in non dolore labore ea amet cillum ea qui dolor nisi sed velit mollit exercitation ex fugiat labore in deserunt culpa laborum culpa anim dolore laboris amet irure mollit proident velit fugiat aute ea elit magna consequat qui officia quis elit Duis dolor esse cupidatat tempor proident voluptate aliqua ex cupidatat do eiusmod veniam irure laborum ut magna nostrud dolore ullamco commodo elit sit magna aliqua laborum veniam officia dolor.
-                </p>
-            </div>
-
+            <Content/>
             <Skills/>
-
         </div>
 
         <div class="row about-content about-content--buttons">
